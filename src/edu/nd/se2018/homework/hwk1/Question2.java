3aa760d12ae87a9c847a741b1e0f0ffeb27b6f76
Brenden Judson <brenden@Brendens-MacBook-Air.local>
2018-09-03 17:00:39 -0400

    hw 1... there was confusion on how to turn in

--- src/edu/nd/se2018/homework/hwk1/Question2.java
+++ src/edu/nd/se2018/homework/hwk1/Question2.java
@@ -1,10 +1,47 @@
-package edu.nd.se2018.homework.hwk1;
+package homework1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 
 public class Question2 {
-
 	public Question2(){}
 	
-	public String getMostFrequentWord(String input, String stopwords){
-		return "";
+	public String getMostFrequentWord(String input_string, String stop_string){
+		HashMap<String,Integer> count_map = new HashMap<String,Integer>();
+		String [] input_words = input_string.split(" ");
+		List<String> stop_words = Arrays.asList(stop_string.split(" "));
+		
+		//create map of the counts
+		for(int i=0;i<input_words.length;i++){
+			//dont count stop words
+			if(!stop_words.contains(input_words[i])){
+				if(count_map.get(input_words[i]) == null){
+					count_map.put(input_words[i], 1);
+				}
+				
+				else {
+					count_map.put(input_words[i], count_map.get(input_words[i]) + 1);
+				}
+			}
+		}
+		
+		int max_value = 0;
+		String max_key = null;
+		
+		//find the key for the max value
+		for(HashMap.Entry<String,Integer> entry : count_map.entrySet()){
+			if(entry.getValue() > max_value){
+				max_value = entry.getValue();
+				max_key = entry.getKey();
+			}
+			
+			else if(entry.getValue() == max_value) {
+				max_key = null;
+			}
+		}
+		
+		return max_key;
 	}
 }
--- src/edu/nd/se2018/homework/hwk1/Question3.java
+++ src/edu/nd/se2018/homework/hwk1/Question3.java
@@ -1,9 +1,32 @@
-package edu.nd.se2018.homework.hwk1;
+package homework1;
+
 public class Question3 {
+	public Question3(){}
 	
-	public Question3(){}	
-	
-    public int getMirrorCount(int[] numbers){
-		return 0;	
+	public int getMirrorCount(int[] input_list) {
+	    int max = 0;
+	    
+	    //find the largest mirror
+	    for(int i=0; i<input_list.length; i++){
+	        int count = 0;
+	        int j = input_list.length - 1;
+	        
+	        while(j >= 0 && i+count < input_list.length){
+	            if(input_list[i + count] == input_list[j]) {
+	                count++;
+	            } 
+	            
+	            else{
+	                max = Math.max(max, count);
+	                count = 0;
+	            }
+	            
+	            j--;
+	        }
+	                                                                
+	        max = Math.max(max, count);
+	    }
+
+	    return max;
 	}
 }
