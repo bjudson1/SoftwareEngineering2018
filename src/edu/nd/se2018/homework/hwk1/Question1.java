ee108b459409d24bdeb8f195b94d8bab41676035
Brenden Judson <brenden@Brendens-MacBook-Air.local>
2018-09-03 17:00:39 -0400

    hw 1... there was confusion on how to turn in

--- src/edu/nd/se2018/homework/hwk1/Question1.java
+++ src/edu/nd/se2018/homework/hwk1/Question1.java
@@ -1,10 +1,22 @@
-package edu.nd.se2018.homework.hwk1;
+package homework1;
+
+import java.util.HashSet;
 
 public class Question1 {
-		
 	public Question1(){}
 	
-	public int getSumWithoutDuplicates(int[] numbers){
-		return 0;	
+	public int getSumWithoutDuplicates(int[] input_list) {
+		int result = 0;
+		HashSet<Integer> h = new HashSet<Integer>();
+		
+		for(int i=0; i<input_list.length; i++){
+			//only count num once
+			if(!h.contains(input_list[i])){
+				h.add(input_list[i]);
+				result+=input_list[i];
+			}
+		}
+		
+		return result;
 	}
 }
